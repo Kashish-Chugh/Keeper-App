e7d95c293b8a95f836d55428d1a0b7d8f7b94c2e
Kashish-Chugh <chughkashish12@gmail.com>
2020-06-30 02:34:31 +0530

    Updates

---
+++ precache-manifest.38beb50179fe440a947f50d8e42766ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63996c06174935e82c34abf83b5c77d1",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "af4654469a3ea1195843",
+    "url": "/Keeper-App/static/js/2.9506575d.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/Keeper-App/static/js/2.9506575d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6808c2473d84e1407a75",
+    "url": "/Keeper-App/static/js/main.cd57daf4.chunk.js"
+  },
+  {
+    "revision": "a1acf0d252c9b30e7767",
+    "url": "/Keeper-App/static/js/runtime-main.1e88c33d.js"
+  }
+]);
